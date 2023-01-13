3b63b9a64d1b93cb13f739c2c8f2c07791a719e5
ShuVeriDa <bashtarov@outlook.com>
2023-01-13 14:00:49 +0300

    created pages

---
+++ src/pages/burgers/cheeseburger.tsx
@@ -0,0 +1,13 @@
+import {NextPage} from "next";
+
+interface ICheeseburgerProps {
+}
+
+const CheeseburgerPage: NextPage<ICheeseburgerProps> = () => {
+  return (
+    <div>
+      Cheeseburger
+    </div>
+  );
+};
+export default CheeseburgerPage
---
+++ src/pages/burgers/index.tsx
@@ -0,0 +1,13 @@
+import {NextPage} from "next";
+
+interface IBurgersProps {
+}
+
+const BurgersPage: NextPage<IBurgersProps> = () => {
+  return (
+    <div>
+      <h1>Наши Бургеры</h1>
+    </div>
+  );
+};
+export default BurgersPage
